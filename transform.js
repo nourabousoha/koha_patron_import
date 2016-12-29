736dd206e1fa3e4bd738ec3d51e8512a4624409d
Sghir Nouredine <sghir@imist.ma>
2016-12-29 10:40:37 +0000

    add quote function

--- transform.js
+++ transform.js
@@ -1,7 +1,12 @@
 const parse = require('csv-parse');
 const fs = require("fs");
 
-const FILE_NAME = 'usagers.csv'
+const FILE_NAME = 'usagersM.csv';
+const indFonc = 6;
+const indDisc = 8;
+const indOrg = 9;
+const indCode =12;
+const indTheme =7;
 let input = fs.readFileSync(FILE_NAME);
 
 var themeCode = function (theme) {
@@ -21,17 +26,30 @@ var themeCode = function (theme) {
     return 'LSHS'
   }
 }
+let allQuoted = function(myArray){
+  for (let i=1;i<myArray.length;i++) {
+    for(let j=0;j < myArray[0].length;j++){
+      myArray[i][j] = '"' + myArray[i][j] + '"';
+    }
+  }
+  return myArray;
+
+}
+//console.log(allQuoted([['a','b,d','c'],[3,'c','d'],['o','l','f']]));
+//console.log([['a','b,d','c'],[3,'c','d'],['o','l','f']]);
 let transform = function (err, output) {
   output[0].push("patron_attributes");
   output[0].push("categorycode");
   output[0].push("branchcode");
   for (let i = 1; i < output.length; i++) {
-    output[i].push('"FONC:' + output[i][6] + ',DISC:' + output[i][8] + ',ORG:' + output[i][9] + ',CODE:' + output[i][12]+'"');
+    output[i].push('FONC:' + output[i][indFonc] + ',DISC:' + output[i][indDisc] + ',ORG:' + output[i][indOrg] + ',CODE:' + output[i][indCode]);
     output[i].push("ADULT");
-    output[i].push(themeCode(output[i][7]));
+    output[i].push(themeCode(output[i][indTheme]));
   }
-  //console.log(output.join('\n'));
-  fs.writeFile('usagers_transformed.csv', output.join('\n'),  function(err) {
+
+ // console.log((allQuoted(output)).join('\n'));
+//}
+  fs.writeFile('usagersM_transformed.csv',allQuoted(output).join('\n'),  function(err) {
    if (err) {
       return console.error(err);
   }});
