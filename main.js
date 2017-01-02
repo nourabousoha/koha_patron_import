8a8e65b732d64a09e67bb4b7f1e9ece8dabae760
Sghir Nouredine <sghir@imist.ma>
2017-01-02 14:42:49 +0000

    refactoring trnsorm function

--- main.js
+++ main.js
@@ -1,61 +1,36 @@
 const parse = require('csv-parse');
 const fs = require("fs");
 const colors = require('colors');
-const trans = require('./transform')
+const transformed = require('./transform')
 // node-getopt oneline example.
 const getopt = require('node-getopt').create([
-  ['S' , 'in=ARG'  , 'Fichier à traiter'],
-  ['S' , 'out=ARG'  , 'Fichier de sortie'],
-  ['h' , 'help'                , 'display this help'],
-  ['v' , 'version'             , 'show version']
+  ['S', 'in=ARG', 'Fichier à traiter'],
+  ['S', 'out=ARG', 'Fichier de sortie'],
+  ['h', 'help', 'display this help'],
+  ['v', 'version', 'show version']
 ]);              // create Getopt instance
-     // bind option 'help' to default action
+// bind option 'help' to default action
 getopt.setHelp(
-  "Usage: node transform.js --in 'input file name --out 'output file name ' ".blue );
+  "Usage: node transform.js --in 'input file name --out 'output file name ' ".blue);
 getopt.bindHelp();
-let opt =getopt.parseSystem(); // parse command line
-const indFonc = 6;
-const indDisc = 8;
-const indOrg = 9;
-const indCode =12;
-const indTheme =7;
+let opt = getopt.parseSystem(); // parse command line
+
 let data;
 let inputFile = opt.options.in;
-let outputFile = opt.options.out ;
-if (undefined === inputFile  | outputFile === undefined ){
-getopt.showHelp();
-process.exit();
+let outputFile = opt.options.out;
+let transform;
+if (undefined === inputFile | outputFile === undefined) {
+  getopt.showHelp();
+  process.exit();
 }
-else{
+else {
   try {
-data = fs.readFileSync(inputFile,'utf8');
+    data = fs.readFileSync(inputFile, 'utf8');
+    transform = transformed(outputFile);
 
-} catch(err) {
-console.log(colors.red('Le fichier  ' + inputFile + ' n\'existe pas ' + err ));
-process.exit();
-}}
-//console.log(allQuoted([['a','b,d','c'],[3,'c','d'],['o','l','f']]));
-//console.log([['a','b,d','c'],[3,'c','d'],['o','l','f']]);
-let transform = function (err, output) {
-  output[0].push("patron_attributes");
-  output[0].push("categorycode");
-  output[0].push("branchcode");
-  for (let i = 1; i < output.length; i++) {
-    output[i].push('FONC:' + output[i][indFonc] + ',DISC:' + output[i][indDisc] + ',ORG:' + output[i][indOrg] + ',CODE:' + output[i][indCode]);
-    output[i].push("ADULT");
-    output[i].push(trans.themeCode(output[i][indTheme]));
-  }
-
- // console.log((allQuoted(output)).join('\n'));
-//}
-  fs.writeFile(outputFile,trans.allQuoted(output).join('\n'),  function(err) {
-   if (err) {
-      return console.error(err);
-  }else{
-    console.log(colors.green('fichier transformé et enrgisté en ' + outputFile));
-  }
-});
-}
+  } catch (err) {
+    console.log(colors.red('Le fichier  ' + inputFile + ' n\'existe pas ' + err));
+    process.exit();  }}
 
 parse(data, transform);
 
--- transform.js
+++ transform.js
@@ -1,31 +1,54 @@
-var themeCode = function (theme) {
-  if (theme === 'Sciences médicales et pharmaceutiques') {
-    return 'SMP'
-  }
-  else if (theme === 'Sciences juridiques économiques et de gestion') {
-    return 'SJEG'
-  }
-  else if (theme === "Sciences de l'ingénieur") {
-    return 'SDI'
-  }
-  else if (theme === 'Sciences exactes et naturelles') {
-    return 'SEN'
-  }
-  else if (theme === 'Lettres et sciences humaines') {
-    return 'LSHS'
-  }
-}
-let allQuoted = function(myArray){
-  for (let i=1;i<myArray.length;i++) {
-    for(let j=0;j < myArray[0].length;j++){
-      myArray[i][j] = '"' + myArray[i][j] + '"';
+const fs = require("fs");
+const indFonc = 6;
+const indDisc = 8;
+const indOrg = 9;
+const indCode = 12;
+const indTheme = 7;
+
+let themeCode = function (theme) {
+    if (theme === 'Sciences médicales et pharmaceutiques') {
+        return 'SMP'
     }
-  }
-  return myArray;
+    else if (theme === 'Sciences juridiques économiques et de gestion') {
+        return 'SJEG'
+    }
+    else if (theme === "Sciences de l'ingénieur") {
+        return 'SDI'
+    }
+    else if (theme === 'Sciences exactes et naturelles') {
+        return 'SEN'
+    }
+    else if (theme === 'Lettres et sciences humaines') {
+        return 'LSHS'
+    }
+}
+let allQuoted = function (myArray) {
+    for (let i = 1; i < myArray.length; i++) {
+        for (let j = 0; j < myArray[0].length; j++) {
+            myArray[i][j] = '"' + myArray[i][j] + '"';
+        }
+    }
+    return myArray;
 
 }
+let transform = function (outputFile) {
+    return function(err, output){
+    output[0].push("patron_attributes");
+    output[0].push("categorycode");
+    output[0].push("branchcode");
+    for (let i = 1; i < output.length; i++) {
+        output[i].push('FONC:' + output[i][indFonc] + ',DISC:' + output[i][indDisc] + ',ORG:' + output[i][indOrg] + ',CODE:' + output[i][indCode]);
+        output[i].push("ADULT");
+        output[i].push(themeCode(output[i][indTheme]));
+    }
 
-module.exports ={
-    themeCode : themeCode,
-    allQuoted: allQuoted
+
+    fs.writeFile(outputFile, allQuoted(output).join('\n'), function (err) {
+        if (err) {
+            return console.error(err);
+        }
+    });
+    }
 }
+
+module.exports = transform;
